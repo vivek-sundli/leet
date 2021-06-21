31b0b4a5d5acb519c4de93a219d6a164900d768b
vivek-sundli <33592335+vivek-sundli@users.noreply.github.com>
2021-06-21 22:27:17 +0530

    Code Snippet for running Array

---
+++ RunningArraySum.cpp
@@ -0,0 +1,13 @@
+//Checking Repo first commit
+class Solution {
+public:
+    vector<int> runningSum(vector<int>& nums) {
+        vector<int> solutionsArray;
+        int i,prev=0;
+        for(i=0;i<nums.size();i++){
+            solutionsArray[i]= prev + nums[i];
+            prev = solutionsArray[i];
+        }
+        return solutionsArray;
+    }
+};
