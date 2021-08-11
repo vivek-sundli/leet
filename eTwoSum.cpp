6845b68f8cde7142b8a01b5e512865e6fd512cdd
vivek-sundli <33592335+vivek-sundli@users.noreply.github.com>
2021-08-11 23:03:31 +0530

    Create eTwoSum.cpp
    pseudo

---
+++ eTwoSum.cpp
@@ -0,0 +1,16 @@
+//twosum
+
+[2,7,11,15] --> 9
+[0,1]
+
+unordered_map[target-i] == found
+make_pair(i,unordered_map[target-i]);
+
+vector<int,int> v;
+v.push_back(make_pair(i,unordered_map[target-i]);
+
+for(int i=0;i<n;i+=){
+	if(){
+		
+	}
+}
