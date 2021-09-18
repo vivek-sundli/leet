9ec97365b55ad1d7717052d0585630c19021894b
vivek-sundli <33592335+vivek-sundli@users.noreply.github.com>
2021-09-19 01:41:20 +0530

    Update emergesort.cpp
    working solution

--- emergesort.cpp
+++ emergesort.cpp
@@ -1,37 +1,30 @@
 class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-        int i,j;
-        vector<int> v(m+n);
-        for(i=0,j=0;i<m || j<n;){
-            if(nums1[i]<nums2[j]){
-                v.push_back(nums1[i]);
+        int i=0,j=0;
+        vector<int> ans;
+        for(;i<m && j<n;){
+            if(nums1[i]<=nums2[j]){
+                ans.push_back(nums1[i]);
                 i++;
-            }
-            if(nums1[i]>nums2[j]){
-                v.push_back(nums2[j]);
+            }else{
+                ans.push_back(nums2[j]);
                 j++;
             }
-            else{
-                v.push_back(nums1[i]);
-                i++;
-            }
         }
-        //when nums1 is fully trversed put nums2 in vector
+        
         if(i==m){
             for(;j<n;j++){
-                v.push_back(nums2[j]);
+                ans.push_back(nums2[j]);
             }
         }
-        //when nums2 is fully trversed put nums1 in vector
         if(j==n){
             for(;i<m;i++){
-                v.push_back(nums1[i]);
+                ans.push_back(nums1[i]);
             }
         }
-        //vector to nums1
-        for(int k=0;k<m+n;k++){
-            nums1[i]=v[k];
+        for(int e=0;e<m+n;e++){
+            nums1[e]=ans[e];
         }
     }
 };
